7acd1fdb0c730e56b149b1f36d298614933910c9
Juan Camilo Marin Navia <jcmnavia@hotmail.com>
2023-03-14 21:48:19 -0500

    Deploy workflow

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'redvalley.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
